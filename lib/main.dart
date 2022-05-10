695819f9850280802e0b5679e86ce25bc111d438
anshb7 <92212789+anshb7@users.noreply.github.com>
2022-05-10 22:22:43 +0530

    search bar added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import './homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController editingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,28 +25,46 @@ class MyApp extends StatelessWidget {
             primaryColor: Color.fromRGBO(35, 35, 35, 10),
             accentColor: Colors.white),
         home: Scaffold(
-          backgroundColor: Color.fromRGBO(35, 35, 35, 10),
-          appBar: AppBar(
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            actions: [
-              GFButton(
-                onPressed: () {},
-                text: """  Want personalized
+            backgroundColor: Color.fromRGBO(35, 35, 35, 10),
+            appBar: AppBar(
+              leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                GFButton(
+                  onPressed: () {},
+                  text: """  Want personalized
   recommendations?""",
-                shape: GFButtonShape.pills,
-                type: GFButtonType.outline,
-                size: GFSize.SMALL,
-                color: GFColors.WHITE,
-                textStyle: TextStyle(
-                    fontFamily: 'Ubuntu',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12),
-                constraints: BoxConstraints(minHeight: 1, minWidth: 1),
-              )
-            ],
-          ),
-        ));
+                  shape: GFButtonShape.pills,
+                  type: GFButtonType.outline,
+                  size: GFSize.SMALL,
+                  color: GFColors.WHITE,
+                  textStyle: TextStyle(
+                      fontFamily: 'Ubuntu',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                  constraints: BoxConstraints(minHeight: 1, minWidth: 1),
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: editingController,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    )),
+              ),
+            )));
   }
 }
