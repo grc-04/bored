94dd11f00ac75307a304a58bfcd696c4e95227c7
anshb7 <92212789+anshb7@users.noreply.github.com>
2022-05-11 11:55:23 +0530

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:bored/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
