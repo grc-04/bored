b6d5c7c38f2e0a4d9059c7ddb7e1ca645fa88269
pranav2811 <powar.pranav29@gmail.com>
2022-05-19 04:36:20 +0530

    stateful draggablesheet

--- lib/movie_screen.dart
+++ lib/movie_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   const MovieScreen({Key? key}) : super(key: key);
 
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
