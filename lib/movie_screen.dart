88b71ad453813b0c1e38c4ca1d15aad8346609d6
anshb7 <92212789+anshb7@users.noreply.github.com>
2022-05-20 04:17:23 +0530

    gg3

--- lib/movie_screen.dart
+++ lib/movie_screen.dart
@@ -1,4 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:bored/provider/movies.dart';
+import 'package:bored/provider/movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 
 class MovieScreen extends StatefulWidget {
   const MovieScreen({Key? key}) : super(key: key);
@@ -10,180 +18,134 @@ class MovieScreen extends StatefulWidget {
 class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
+    final _movie = Provider.of<Movies>(context, listen: false);
+    final _mlist = _movie.movies;
+    int i = 0;
+
     return Scaffold(
-      body: Stack(children: [
-        Image(
-          // ignore: prefer_const_constructors
-          image: NetworkImage(
-              "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"),
-          // fit: BoxFit.cover,
-          width: double.infinity,
-        ),
+      backgroundColor: Colors.black,
+      appBar: AppBar(backgroundColor: Colors.black),
+      body: Stack(fit: StackFit.expand, children: [
+        for (int i = 0; i < _mlist.length; i++)
+          ListView.builder(
+            itemCount: 1,
+            shrinkWrap: true,
+            itemBuilder: (context, i) => Image(
+              // ignore: prefer_const_constructors
+              image: NetworkImage(_mlist[i].imageUrl),
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
         DraggableScrollableSheet(
-          initialChildSize: 0.5,
-          minChildSize: 0.5,
-          maxChildSize: 1.0,
-          builder: (BuildContext context, ScrollController scrollController) {
-            return Column(
-              children: [
-                Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 1.0),
-                      child: Container(
-                        height: 400,
-                        width: 500,
-                        decoration: const BoxDecoration(
-                          color: Color.fromRGBO(38, 33, 48, 0.97),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30),
+            expand: true,
+            initialChildSize: 0.5,
+            minChildSize: 0.5,
+            maxChildSize: 1.0,
+            builder: (context, controller) => ClipRRect(
+                  borderRadius: BorderRadius.circular(24),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.black,
+                    child: SingleChildScrollView(
+                        child: ListView.builder(
+                      controller: controller,
+                      shrinkWrap: true,
+                      itemCount: 1,
+                      itemBuilder: (context, index) => Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(40),
+                                    child: Image.network(
+                                      "https://i.gadgets360cdn.com/large/disney_plus_hotstar_logo_1583901149861.jpg",
+                                      height: 120,
+                                      width: 120,
+                                      alignment: Alignment.center,
+                                    ),
+                                  ))
+                            ],
                           ),
-                        ),
-                        child: Column(
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.only(top: 30.0, right: 80),
-                              child: Text(
-                                'Avengers:Endgame ',
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Title : ",
                                 style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontFamily: 'Ubuntu',
+                                    fontSize: 25),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  _mlist[index].title,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Ubuntu',
+                                      fontSize: 25),
                                 ),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 30.0, left: 80),
-                              child: Row(
-                                children: const [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(10),
-                                      topRight: Radius.circular(10),
-                                      bottomLeft: Radius.circular(10),
-                                      bottomRight: Radius.circular(10),
-                                    ),
-                                    child: Image(
-                                      image: NetworkImage(
-                                          'https://media-exp1.licdn.com/dms/image/C4E0BAQEVb0ZISWk8vQ/company-logo_200_200/0/1519896425167?e=2147483647&v=beta&t=xztAGLJ8SUh-lHaDSK-a3cxYCfSYAy8wrPg3jTx15W8'),
-                                      height: 90,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 50,
-                                  ),
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(10),
-                                      topRight: Radius.circular(10),
-                                      bottomLeft: Radius.circular(10),
-                                      bottomRight: Radius.circular(10),
-                                    ),
-                                    child: Image(
-                                      image: NetworkImage(
-                                          'https://yt3.ggpht.com/3b9SS-66t6V_AfGgaiTgaylOvn6cB8GUBnGV0Q2DMn9PWn6MIzzseiMz-d0UpGrQdhEMl3AwT38=s900-c-k-c0x00ffffff-no-rj'),
-                                      height: 90,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 30.0, left: 20),
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    'Runtime:',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 40,
-                                  ),
-                                  Text(
-                                    '3h 2m',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 30.0, left: 20),
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    'Director:',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 40,
-                                  ),
-                                  Text(
-                                    'Anthony Russo',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(right: 34.0),
-                              child: Text(
-                                'Joe Russo',
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Rating : ",
                                 style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                    fontFamily: 'Ubuntu',
+                                    fontSize: 25),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  _mlist[index].rating.toString(),
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Ubuntu',
+                                      fontSize: 25),
                                 ),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Description : ",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Ubuntu',
+                                    fontSize: 25),
                               ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 30.0, left: 20),
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    'Producer:',
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    _mlist[index].description,
+                                    maxLines: 20,
                                     style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                                        color: Colors.white,
+                                        fontFamily: 'Ubuntu',
+                                        fontSize: 20),
                                   ),
-                                  SizedBox(
-                                    width: 40,
-                                  ),
-                                  Text(
-                                    'Kevin Fiege',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                )
-              ],
-            );
-          },
-        ),
+                    )),
+                  ),
+                )),
       ]),
     );
   }
