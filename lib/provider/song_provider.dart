cc11fd71ce55d55d7f3a21f9284986402d72c329
pranav2811 <powar.pranav29@gmail.com>
2022-05-21 20:27:26 +0530

    fixed images in song

--- lib/provider/song_provider.dart
+++ lib/provider/song_provider.dart
@@ -10,37 +10,37 @@ class Songs with ChangeNotifier {
         artist: "Justin Bieber",
         rating: 8.4,
         imageUrl:
-            "http://cdn.shopify.com/s/files/1/0330/2175/5451/products/The-Kid-Laroi-Justin-Bieber---Stay-FL-Studio-Remake_1200x1200.png?v=1629758334"),
+            "https://i1.sndcdn.com/artworks-NTWumskIAtzxndKO-yz1ryA-t500x500.jpg"),
     Song(
         title: "Industry Baby",
         artist: "Lil Nas X",
         rating: 7.3,
         imageUrl:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.stereogum.com%2F2154748%2Flil-nas-x-industry-baby-feat-jack-harlow%2Fmusic%2F&psig=AOvVaw3SX6f0hZWFdUY2z31Jzfpc&ust=1652615389779000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLjqxKD23vcCFQAAAAAdAAAAABAt"),
+            "https://i1.sndcdn.com/artworks-j1zGqEwf1STpMyZK-PRpXPg-t500x500.jpg"),
     Song(
         title: "First Class",
         artist: "Jack Harlow",
         rating: 7.8,
         imageUrl:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fopen.spotify.com%2Falbum%2F4uVXrwE4aSV2L2aqAHSOXa&psig=AOvVaw367ON6KfVDUdSI2gD1Vy11&ust=1652615551164000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjF0On23vcCFQAAAAAdAAAAABAJ"),
+            "https://i.scdn.co/image/ab67616d0000b2730fbbde391655703a7c8cdc79"),
     Song(
         title: "WAP",
         artist: "Megan Thee Stallion",
         rating: 7.4,
         imageUrl:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allure.com%2Fstory%2Fcardi-b-megan-thee-stallion-wap-cover-90s-updo-hairstyle&psig=AOvVaw1srtBf3_tTET07wBa8KlSD&ust=1652615605719000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIiGj4T33vcCFQAAAAAdAAAAABAD"),
+            "https://pyxis.nymag.com/v1/imgs/3ed/586/1116cbade422ceefc6fa96110bfc56e0d1-cardi-b-megan-thee-stallion-wap.rsquare.w700.jpg"),
     Song(
         title: "abcdefu",
         artist: "Gayle",
         rating: 8.0,
         imageUrl:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpeermusic.com.br%2F2022%2F03%2F11%2Fgayle-abcdefu%2F&psig=AOvVaw0FSPY7OryHYEB21QysQAcz&ust=1652615741763000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLiPjsf33vcCFQAAAAAdAAAAABAI"),
+            "https://i1.sndcdn.com/artworks-ifja3I2KBZJYQRNS-IFu4iQ-t500x500.jpg"),
     Song(
         title: "Rolling in the Deep",
         artist: "Adele",
         rating: 8.1,
         imageUrl:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsoundcloud.com%2Frevelic%2Frevelic-x-adele-rolling-in-the-deepest-remix-1&psig=AOvVaw2ERjLSGTVPcmw1-Uf29V6G&ust=1652615831639000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPidoPD33vcCFQAAAAAdAAAAABAD"),
+            "https://i1.sndcdn.com/artworks-000109026918-2n3ayn-t500x500.jpg"),
   ];
   List<Song> get songs {
     return [..._songs];
