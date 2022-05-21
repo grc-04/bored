ee48b05e492d9d6e350d738699799ed2d940d0e3
pranav2811 <powar.pranav29@gmail.com>
2022-05-21 21:20:37 +0530

    new settings ui

---
+++ lib/settings_ui.dart
@@ -0,0 +1,6 @@
+library settings_ui;
+
+export 'package:bored/src/settings_section.dart';
+export 'package:bored/src/settings_tile.dart';
+export 'package:bored/src/settings_list.dart';
+export 'package:bored/src/custom_section.dart';
